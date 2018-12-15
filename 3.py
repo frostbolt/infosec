c57851a524fdcb1426a0a1cf0cb6c4ff905e5ddd
Sergey Luschik <frostbolt@live.ru>
2018-12-16 00:50:53 +0300

    change lab3 implementation. implement lab4

--- 3.py
+++ 3.py
@@ -1,42 +1,35 @@
 # Шифр Виженера
 import random
-ALPHABET = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+ALPHABET = 'АБВГДЕЖЗИЙКЛМОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 STRING_TO_ENCODE = "НЕБЫВАЛЫЕСКИДКИНАВСЕКРИПТОВАЛЮТЫ"
 
 def generate_key(length):
     result = ""
     for i in range(length):
         result += ALPHABET[random.randint(0, len(ALPHABET) - 1)]
+
     return result
 
-KEYWORD = generate_key(random.randint(0, len(STRING_TO_ENCODE) - 1))
+KEYWORD = generate_key(random.randint(1, len(STRING_TO_ENCODE) - 1))
 
 def vigenure(text, key, step):
     result_string = ""
 
     for i in range(len(text)):
         a = ALPHABET.find(text[i])
-        if a == -1:
-            raise Exception("Алфавит не содержит " + text[i])
         b = ALPHABET.find(key[i % len(key)])
 
-        if b == -1:
-            raise Exception("Алфавит не содержит " + key[i])
+        if a < 0:
+            result_string += text[i]
+        else:
+            result_string += ALPHABET[(a + b*step) % len(ALPHABET)]
 
-        c = (a + b*step) % len(ALPHABET)
-        result_string += ALPHABET[c]
     return result_string
 
+ENCODED_STRING = vigenure(STRING_TO_ENCODE, KEYWORD, 1)
+DECODED_STRING = vigenure(ENCODED_STRING, KEYWORD, -1)
 
-try:
-	ENCODED_STRING = vigenure(STRING_TO_ENCODE, KEYWORD, 1)
-	DECODED_STRING = vigenure(ENCODED_STRING, KEYWORD, -1)
-
-except Exception as e:
-	print(e)
-
-else:
-	print(KEYWORD)
-	print(ENCODED_STRING)
-	print(DECODED_STRING)
-	print(DECODED_STRING == STRING_TO_ENCODE)
+print(KEYWORD)
+print(ENCODED_STRING)
+print(DECODED_STRING)
+print(DECODED_STRING == STRING_TO_ENCODE)
